722086e6dd86390c76601f541091a56e87217307
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-06-20 11:05:21 +0800

    feat: use basic template from official documentation.

--- hello.go
+++ hello.go
@@ -1,5 +1,19 @@
-package main
+package caddy2_redirect
 
-func main() {
-	println("Hello")
+import "github.com/caddyserver/caddy/v2"
+
+func init() {
+	caddy.RegisterModule(Gizmo{})
+}
+
+// Gizmo is an example; put your own type here.
+type Gizmo struct {
+}
+
+// CaddyModule returns the Caddy module information.
+func (Gizmo) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "foo.gizmo",
+		New: func() caddy.Module { return new(Gizmo) },
+	}
 }
